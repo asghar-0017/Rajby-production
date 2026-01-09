dc038ae10deea9bbb362a5a44d4983da45eb732e
asgharali62 <asghar.ali@xperlabs.com>
2026-01-10 03:53:55 +0500

    .

--- test-bulk-upload.js
+++ test-bulk-upload.js
@@ -52,7 +52,7 @@ const testBulkUpload = async () => {
     });
 
     const response = await fetch(
-      "http://157.245.150.54:5155/api/tenant/tenant_1756409312403_uo1hmt4tz/invoices/bulk",
+      "http://143.198.95.2:5155/api/tenant/tenant_1756409312403_uo1hmt4tz/invoices/bulk",
       {
         method: "POST",
         headers: {
